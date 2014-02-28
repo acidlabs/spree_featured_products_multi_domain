acba0f31b9e0b2246138d68a8f6d474c10588313
Ignacio Mella <ignacio@acid.cl>
2014-02-28 15:25:25 -0300

    Added fix for variant images

--- app/models/spree/featured_product.rb
+++ app/models/spree/featured_product.rb
@@ -3,6 +3,7 @@ class Spree::FeaturedProduct < ActiveRecord::Base
   belongs_to :store
   delegate :images, to: :product
   delegate :name, to: :product
+  delegate :variant_images, to: :product
   delegate :name, to: :store, prefix: true
 
   scope :by_store, -> (store_id) { where(store_id: store_id ) }
